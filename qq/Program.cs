9568da43ba31a618b4756c3f9667bddfd9f6a733
MohamedNasr <mohamednasr27122@gmail.com>
2026-03-04 21:06:17 +0200

    Part 1 Question 3

--- qq/Program.cs
+++ qq/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -29,6 +31,14 @@ namespace qq
             //The method must be called through the interface reference like((IEnglishSpeaker) translator).Greet().
             #endregion
 
+            #region Q3
+            //A shallow copy creates a new object but copies the references of nested objects, so both objects share the same referenced data.
+            //A deep copy creates a new object and also copies all nested objects, making the copy completely independent.
+
+            //Shallow copy is used when sharing references is acceptable and copying needs to be fast.Deep copy is used when the copied object must be completely independent.
+
+            //The risk of shallow copy is that changes to reference-type fields in one object will affect the other object since they share the same referenced objects.
+            #endregion
             #endregion
         }
     }
